2cd025c41d3cd978abc4acd17ef68e7deb776e90
luhan <luhansantanna@gmail.com>
2021-03-02 00:12:23 -0300

    Project almost done. Necessita integração com Mysql e cuidar a segurança de valida_login

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -6,9 +6,29 @@
     $arquivo = fopen('arquivo.hd', 'r'); // 'r' to read
 
 // feof -> testa fim do arquivo e retorn false se não encontro o fim na linha
-    while(!feof($arquivo)) {
-        $registro = fgets($arquivo); // pega toda a linha onde está o ponteiro no momento
-        $chamados[] = $registro;
+//enquanto houver registros (linhas) a serem recuperados
+    while(!feof($arquivo)) { //testa pelo fim de um arquivo
+        //linhas
+        $registro = fgets($arquivo);
+
+        //explode dos detalhes do registro para verificar o id do usuário responsável pelo cadastro
+        $registro_detalhes = explode('#', $registro);
+
+        //(perfil id = 2) só vamos exibir o chamado, se ele foi criado pelo usuário
+        if($_SESSION['perfil_id'] == 2) {
+
+            //se usuário autenticado não for o usuário de abertura do chamado então não faz nada
+            if($_SESSION['id'] != $registro_detalhes[0]) {
+                continue; //não faz nada
+
+            } else {
+                $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+            }
+
+        } else {
+            $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+        }
+
     }
 
     fclose($arquivo);
@@ -60,14 +80,6 @@
 
                   <? $c_dados = explode('#', $c);
 
-                  //verificar se perfil é adm
-                  if($_SESSION['perfil_id'] == 2) {
-                      // exibir chamados somente se foi criado pelo usuário
-                      if($_SESSION['perfil_id'] != $c_dados[0]) {
-                          continue;
-                      }
-                  }
-
                   if(count($c_dados) < 3) { // faltar alguma info (pula interação)
                       continue;
                   }
@@ -75,9 +87,9 @@
 
               <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title"><?= $c_dados[1]; ?></h5> <!-- omitir o id -->
-                  <h6 class="card-subtitle mb-2 text-muted"><?= $c_dados[2]; ?></h6>
-                  <p class="card-text"><?= $c_dados[3]; ?></p>
+                  <h5 class="card-title"><?= $c_dados[0]; ?></h5> <!-- omitir o id -->
+                  <h6 class="card-subtitle mb-2 text-muted"><?= $c_dados[1]; ?></h6>
+                  <p class="card-text"><?= $c_dados[2]; ?></p>
                 </div>
               </div>
 
